37313bc1fd3f0c428b110b2f3e52a0e7aba91fb6
jaffourt <jaffourt@mit.edu>
2020-06-06 09:34:59 -0400

    Nikola auto commit.
    Nikola version: 8.0.4

--- conf.py
+++ conf.py
@@ -153,7 +153,7 @@ NAVIGATION_ALT_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "bootstrap3-gradients"
+THEME="bootstrap3-gradients"
 
 # Primary color of your theme. This will be used to customize your theme.
 # Must be a HEX value.
